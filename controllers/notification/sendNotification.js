7eceffa02bb36e5b846f75b0db13472a370a8826
Atullsenn <atulsen0123@gmail.com>
2022-12-09 18:44:10 +0530

    commit 12/9/2022 7:05

--- controllers/notification/sendNotification.js
+++ controllers/notification/sendNotification.js
@@ -2,6 +2,23 @@ const db = require('../../db/dbConnection');
 
 const sendNotification = (req,res)=>{
 
+    if(req.body.heading == '' || req.body.heading == null){
+        res.status(400).send({
+            success: false,
+            message: 'Please Enter Heading'
+        })
+        return;
+    }
+
+
+    if(req.body.description == '' || req.body.description == null){
+        res.status(400).send({
+            success: false,
+            message: 'Please Enter Description'
+        })
+        return;
+    }
+
     db.query(`INSERT INTO tbl_notifications (heading,description,hunt_id) 
     VALUES ('${req.body.heading}','${req.body.description}','${req.body.hunt_id}');`,(err,data)=>{
         if(err){
